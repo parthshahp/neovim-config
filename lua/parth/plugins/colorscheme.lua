88d5d60f637ba2bd4ea4a1849bbb65b747cd3959
Parth Shah <parthshahp1@gmail.com>
2024-03-25 23:36:14 -0400

    Cleanup

--- lua/parth/plugins/colorscheme.lua
+++ lua/parth/plugins/colorscheme.lua
@@ -3,11 +3,11 @@ return {
     'sainnhe/gruvbox-material',
     lazy = true,
     priority = 1000,
-    config = function() vim.print("setup gruvbox-material") end,
+    -- config = function() vim.print("setup gruvbox-material") end,
   },
   {
     "rebelot/kanagawa.nvim",
     lazy = true,
-    config = function() vim.print("setup kanagawa") end,
+    -- config = function() vim.print("setup kanagawa") end,
   }
 }
--- lua/parth/plugins/fugitive.lua
+++ lua/parth/plugins/fugitive.lua
@@ -1,4 +1,5 @@
 return { 'tpope/vim-fugitive',
          'tpope/vim-sleuth',
-         'numtoStr/Comment.nvim', opts ={}, lazy=true
+         'numtoStr/Comment.nvim', opts ={}, lazy=true,
+         'nvim-tree/nvim-web-devicons'
      }
