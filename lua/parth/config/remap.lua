ad47214a6bb81c3f99a0b3a91c634cb278459a89
Parth Shah <parthshahp1@gmail.com>
2024-04-01 03:17:13 -0400

    Refactor more and add conform

--- lua/parth/config/remap.lua
+++ lua/parth/config/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'Open NetRW' })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open NetRW" })
 
 -- Move highlighted lines around
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -14,14 +14,14 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Delete to void buffer
-vim.keymap.set("x", "<leader>p", "\"_dP", { desc = 'Overwrite to void buffer'} )
-vim.keymap.set("n", "<leader>d", "\"_d", { desc = 'Delete to void buffer'})
-vim.keymap.set("v", "<leader>d", "\"_d", { desc = 'Delete to void buffer'})
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Overwrite to void buffer" })
+vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete to void buffer" })
+vim.keymap.set("v", "<leader>d", '"_d', { desc = "Delete to void buffer" })
 
 -- System clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
 -- Window key
 -- vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', { noremap = true })
@@ -31,20 +31,25 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 -- vim.keymap.set('n', '<M-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- vim.keymap.set('n', '<M-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = 'Open Diagnostic Float', noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Open Git Fugitive Window' } )
-vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {desc = 'Open Git Preview Hunk'})
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open Git Fugitive Window" })
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Open Git Preview Hunk" })
+vim.keymap.set({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find File" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Old Files" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
---
+++ lua/parth/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				go = { "gofumpt", "golines" },
+				html = { "prettier" },
+				javascript = { "prettier" },
+				css = { "prettier" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+				async = false,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file" })
+	end,
+}
